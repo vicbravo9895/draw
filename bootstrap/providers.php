2c251678278d286a5e6e18bcf0548cc81600e5c0
vicbravo9895 <vicbravo@delapengineering.com>
2026-02-20 00:25:52 -0600

    Register TelescopeServiceProvider conditionally based on package availability

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,5 +4,8 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\FortifyServiceProvider::class,
     App\Providers\HorizonServiceProvider::class,
-    App\Providers\TelescopeServiceProvider::class,
+    // Telescope es require-dev: solo registrar si el paquete está instalado (p. ej. en Docker --no-dev no está)
+    ...(class_exists(\Laravel\Telescope\TelescopeApplicationServiceProvider::class)
+        ? [App\Providers\TelescopeServiceProvider::class]
+        : []),
 ];
